46f3381f9bfd48cd75724bcf9cd715ff0234dbc8
kaz-i-54 <torune.summer@gmail.com>
2020-04-12 20:30:58 +0900

    3.1, 3.2のやり直し

--- 03_learning/m3_2.py
+++ 03_learning/m3_2.py
@@ -1,58 +1,86 @@
 from sklearn.metrics import confusion_matrix, roc_curve
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, KFold, train_test_split
 from sklearn.svm import SVC
 
 from m3_1 import eval_model, preapare_dataset
 
 
-def grid(X, Y, params, model, n_jobs=-1):
-    #     kf = KFold(n_splits=10, shuffle=True, random_state=42)
+def grid(X, Y, params, model, scoring='neg_mean_squared_error',  n_jobs=-1):
+    """
+    グリッドサーチをする
+    :param X:
+    :param Y:
+    :param params:
+    :param model:
+    :param scoring:
+    :param n_jobs:
+    :return:
+    """
+    kf = KFold(n_splits=10, shuffle=True, random_state=42)
 
-    gs = GridSearchCV(estimator=model,
-                      param_grid=params,
-                      cv=10,
-                      n_jobs=n_jobs)
+    # TODO: 評価手法をAUROCとF-Scoreの二通りで試す
+    gs = GridSearchCV(
+        estimator=model,
+        param_grid=params,
+        cv=kf,
+        scoring=scoring,
+        n_jobs=n_jobs)
 
     gs.fit(X, Y)
     print(gs.best_params_)
 
-    # 評価
-    y_pred_gs = gs.predict(X)
-    confusion_matrix(Y, y_pred_gs)
-
-    y_score_gs = gs.predict_proba(X)
-
-    eval_model(Y, y_pred_gs, y_score_gs)
-
     return gs
+
+
+def regression(model, x, y):
+    """
+
+    :param model:
+    :param x:
+    :param y:
+    :return:
+    """
+    X_train, X_test, Y_train, Y_test = train_test_split(x, y, shuffle=True)
+    model.fit(X_train, Y_train)
+
+    y_pred = model.predict(X_test)
+    y_score = model.predict_proba(X_test)
+
+    eval_model(Y_test, y_pred, y_score)
+    return model
+
+
+def svc_p_search_and_score(X, Y, params, method):
+    svm = SVC(kernel='rbf', probability=True)
+    gs = grid(X, Y, params, svm, method)
+
+    svm_b = SVC(kernel='rbf', probability=True, **gs.best_params_)
+    regression(svm_b, X, Y)
+
+    return svm_b
+
+
+
 
 if __name__ == '__main__':
 
     X, Y = preapare_dataset("data/ionosphere.csv")
-    # candidate_params = {
-    #     'C': [1, 10, 100, 1000],
-    #     'gamma': [00.1, 0.1, 1, 10],
-    # }
-    #
-    # svm = SVC(kernel='rbf', probability=True)
-    #
-    # gs = grid(X, Y, candidate_params, svm)
-    #
 
-    params2 = {
+    params = {
         'C': list(range(50, 150, 20)),
         'gamma': [i / 10 for i in range(1, 6, 1)],
     }
 
-    svm = SVC(kernel='rbf', probability=True)
-    gs2 = grid(X, Y, params2, svm)
+
+    method1 = 'roc_auc'
+    method2 = 'f1'
+
+    print("==================" + method1 + "==================" )
+    svc_p_search_and_score(X, Y, params, method1)
+
+    print("==================" + method2 + "==================" )
+    svc_p_search_and_score(X, Y, params, method2)
 
 
-# {'C': 50, 'gamma': 0.2}
-# precision score of test set: 1.00000
-# recall score of test set: 1.00000
-# auc score of test set: 1.00000
-# matthews_corrcoef score of test set: 1.00000
-# f score of test set: 1.00000
 
 
--- README.md
+++ README.md
@@ -10,30 +10,51 @@
 
 # 3. 機械学習
 .scaleのファイルの処理方法が分からなかったので、kaggleで検索してcsvのファイルをダウンロードした。
+## 3.1 結果を以下に記載する
 
+precision score of test set: 0.89919  
+recall score of test set: 0.99111  
+auc score of test set: 0.98106  
+matthews_corrcoef score of test set: 0.83510  
+f score of test set: 0.94292  
+## 3.2 結果を以下に記載する
+==================roc_auc==================  
+{'C': 50, 'gamma': 0.2}  
+precision score of test set: 0.98077  
+recall score of test set: 0.91071  
+auc score of test set: 0.96429  
+matthews_corrcoef score of test set: 0.86047  
+f score of test set: 0.94444  
+==================f1==================  
+{'C': 50, 'gamma': 0.1}  
+precision score of test set: 0.94340  
+recall score of test set: 0.92593  
+auc score of test set: 0.97495  
+matthews_corrcoef score of test set: 0.83339  
+f score of test set: 0.93458  
 # 4. 創薬情報処理
 2d記述子、3d記述子、fingerprintは04chem/m4_1-4.ipynbで用意した。
 機械学習はそれぞれの.pyで実行して、結果をresultに記載した。
 
 最も良かったのは2D記述子を使って、lightGBMで学習したモデルでハイパーパラメータは
 
-#　{'learning_rate': 0.05, 'max_depth': 25, 'n_estimators': 200, 'num_leaves': 100}
-mean rmse: 0.47811
-mean r2: 0.82085
+{'learning_rate': 0.05, 'max_depth': 25, 'n_estimators': 200, 'num_leaves': 100}  
+mean rmse: 0.47811  
+mean r2: 0.82085  
 のようになった。
 
 
 ### 4.8 考察
 それぞれの化合物の予測のスコアをLogPappの実験値と予測値の差で示す。
-予測のスコアとAssayIDに関連はない。
+予測のスコアとAssayIDに関連はない。 
 04_chem/result/diff_test には予測のスコアがが良いものと悪いものの描画を保存する。一見して２つの画像間から推測できることはない。
 
 ### 4.9 発展
 次元削減をして2d, 3d, fingerprintをあわせた。(4_9.py)
 optunaでパラメーター探索したスコアは
-Best is trial#5 with value: 0.5431523303034916.
-{'bagging_freq': 2, 'min_data_in_leaf': 11, 'max_depth': 30, 'learning_rate': 0.06322568717428279, 'num_leaves': 486, 'num_threads': 8, 'min_sum_hessian_in_leaf': 8}
+Best is trial#5 with value: 0.5431523303034916.  
+{'bagging_freq': 2, 'min_data_in_leaf': 11, 'max_depth': 30, 'learning_rate': 0.06322568717428279, 'num_leaves': 486, 'num_threads': 8, 'min_sum_hessian_in_leaf': 8}  
 
-mean rmse: 0.54587
+mean rmse: 0.54587  
 mean r2: 0.76672
 となった。
