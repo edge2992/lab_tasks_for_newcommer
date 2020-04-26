8c9d4d17487ca869e690aeb2a0de269e3cd9a060
kaz-i-54 <torune.summer@gmail.com>
2020-04-26 18:18:44 +0900

    #2 centerofmass

--- 02_protein/m2_8.py
+++ 02_protein/m2_8.py
@@ -4,8 +4,10 @@ filename = "data/1buw.pdb"
 cmd.load(filename)
 cmd.hide("everything")
 c = cmd.centerofmass("chain A")
-
-cmd.select("inside", "chain A within {:.2f} of center chain A".format(calc_gc(filename, "chain A")))
+cmd.origin(position=c)
+cmd.center('origin')
+print(cmd.get_position())
+cmd.select("inside", "chain A within {:.2f} of origin".format(calc_gc(filename, "chain A")))
 cmd.select("outside", "not inside")
 
 cmd.color("red", "inside")
